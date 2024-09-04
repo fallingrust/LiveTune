8e3ebc8bcb3b67ad2a3fb72a7fa77f7fd8954220
雪健 陈 <23722021@qq.com>
2024-09-04 18:01:15 +0800

    update

---
+++ LiveTune/Views/MainView.axaml.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia.Controls;
+
+namespace LiveTune.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+
+    private void OnTitleBarPointerPressed(object? sender, Avalonia.Input.PointerPressedEventArgs e)
+    {
+        (TopLevel.GetTopLevel(this) as Window)?.BeginMoveDrag(e);
+    }  
+}
---
+++ LiveTune/Views/MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Controls;
+
+namespace LiveTune.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        ExtendClientAreaToDecorationsHint = true;
+        ExtendClientAreaChromeHints = Avalonia.Platform.ExtendClientAreaChromeHints.NoChrome;
+    }
+}
