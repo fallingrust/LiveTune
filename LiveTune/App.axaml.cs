89ec00950066f809585980ee04f7d22af5aef6c8
雪健 陈 <23722021@qq.com>
2024-09-11 10:11:32 +0800

    update vlc

--- LiveTune/App.axaml.cs
+++ LiveTune/App.axaml.cs
@@ -2,11 +2,9 @@
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
-using LibVLCSharp.Shared;
 using LiveTune.ViewModels;
 using LiveTune.Views;
 using LiveTune.Views.Windows;
-using System;
 using System.Globalization;
 using System.Runtime.InteropServices;
 
@@ -22,11 +20,7 @@ public partial class App : Application
     public override void OnFrameworkInitializationCompleted()
     {
         AllocConsole();
-        Assets.Resources.Culture = new CultureInfo("zh");
-       var _vlc = new LibVLC(true);
-      var  _media = new Media(_vlc, new Uri("http://as-hls-ww-live.akamaized.net/pool_904/live/ww/bbc_radio_five_live/bbc_radio_five_live.isml/bbc_radio_five_live-audio%3d96000.norewind.m3u8"));
-      var  _player = new MediaPlayer(_media);
-        _player.Play();
+        Assets.Resources.Culture = new CultureInfo("zh");       
         BindingPlugins.DataValidators.RemoveAt(0);       
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
